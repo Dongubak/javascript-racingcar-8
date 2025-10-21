3301a4cf17595a0060673fb1d10be989eaf1d097
Dongubak <x001125@gmail.com>
2025-10-21 16:50:58 +0900

    test: add unit test skeletons for isNotEmptyString and splitWithSeparator
    - isNotEmptyString 단위 모듈 테스트 케이스 기본 구조 추가
    - splitWithSeparator 단위 모듈 테스트 케이스 기본 구조 추가
    - 테스트 본문은 추후 구현 예정 (입력 및 기대값 검증 로직 미작성 상태)

---
+++ __tests__/parseInputString.test.js
@@ -0,0 +1,10 @@
+describe('isNotEmptyString 단위 모듈 테스트', () => {
+  test("'Jaspers,sanchez' 입력", () => {});
+  test('빈 문자열 입력', () => {});
+});
+
+describe('splitWithSeparator 단위 모듈 테스트', () => {
+  test("'Jaspers,sanchez' 입력", () => {});
+  test("'Jaspers,sanchez,John' 입력", () => {});
+  test('빈 문자열 입력', () => {});
+});
